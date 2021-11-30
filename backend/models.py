d9550dd99fb8c81fed1aceaa2842d5df77dfac8b
Nikita P <36228624+commanderprice@users.noreply.github.com>
2021-11-30 17:00:03 +0300

    Add traceability and User tables

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,11 @@
 import enum
-from datetime import datetime
 from typing import List, Union, Optional, NamedTuple
 
 from sqlalchemy import (
-    select, Enum, DateTime,
+    select, Enum, DateTime, Date,
     Boolean, Column, ForeignKey, 
     Integer, String, Float)
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.orm.session import Session
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.sql.schema import UniqueConstraint
@@ -177,3 +176,227 @@ class AttributeDefinition(Base):
     __table_args__ = (
         UniqueConstraint('schema_id', 'attribute_id'),
     )
+
+
+# class Group(Base):
+#     __tablename__ = 'groups'
+
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(128), nullable=False)
+#     parent_id = Column(Integer, ForeignKey('groups.id'))
+#     parent = relationship('Group', remote_side=[id], backref=backref('subgroups'))
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(128), unique=True, nullable=False)
+    email = Column(String(128), unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+# class PermObject(enum.Enum):
+#     SCHEMA = 'SCHEMA'
+#     ENTITY = 'ENTITY'
+
+# class PermType(enum.Enum):
+#     CREATE = 'CREATE'
+#     UPDATE = 'UPDATE'
+#     DELETE = 'DELETE'
+
+#     CREATE_ENTITIES = 'CREATE_ENTITIES'
+#     UPDATE_ENTITIES = 'UPDATE_ENTITIES'
+#     DELETE_ENTITIES = 'DELETE_ENTITIES'
+
+
+# class Permission(Base):
+#     __tablename__ = 'permissions'
+#     id = Column(Integer, primary_key=True)
+#     obj_id = Column(Integer)
+#     obj_type = Column(Enum(PermObject), nullable=False)
+#     name = Column(Enum(PermType), nullable=False)
+
+
+# class GroupPermission(Base):
+#     __tablename__ = 'group_permissions'
+#     id = Column(Integer, primary_key=True)
+#     group_id = Column(Integer, ForeignKey('groups.id'), nullable=False)
+#     permission_id = Column(Integer, ForeignKey('permissions.id'), nullable=False)
+
+#     group = relationship('Group')
+#     permission = relationship('Permission')
+
+#     __table_args__ = (
+#         UniqueConstraint('group_id', 'permission_id'),
+#     )
+
+
+# class UserPermission(Base):
+#     __tablename__ = 'user_permissions'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+#     permission_id = Column(Integer, ForeignKey('permissions.id'), nullable=False)
+
+#     user = relationship('User')
+#     permission = relationship('Permission')
+
+#     __table_args__ = (
+#         UniqueConstraint('user_id', 'permission_id'),
+#     )
+
+
+# class UserGroup(Base):
+#     __tablename__ = 'user_groups'
+#     id = Column(Integer, primary_key=True)
+#     group_id = Column(Integer, ForeignKey('groups.id'), nullable=False)
+#     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+#     group = relationship('Group')
+#     user = relationship('User')
+
+#     __table_args__ = (
+#         UniqueConstraint('user_id', 'group_id'),
+#     )
+
+class ChangeStatus(enum.Enum):
+    PENDING = 'PENDING'
+    DECLINED = 'DECLINED'
+    APPROVED = 'APPROVED'
+
+
+class ChangeObject(enum.Enum):
+    SCHEMA = 'SCHEMA'
+    ENTITY = 'ENTITY'
+
+class ChangeType(enum.Enum):
+    CREATE = 'CREATE'
+    UPDATE = 'UPDATE'
+    DELETE = 'DELETE'
+
+class Change(Base):
+    __tablename__ = 'changes'
+    id = Column(Integer, primary_key=True)
+    created_by_user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    reviewed_by_user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime(timezone=True), nullable=False)
+    reviewed_at = Column(DateTime(timezone=True), nullable=True)
+    status = Column(Enum(ChangeStatus), default=ChangeStatus.PENDING, nullable=False)
+    change_object = Column(Enum(ChangeObject), nullable=False)
+    change_type = Column(Enum(ChangeType), nullable=False)
+    comment = Column(String(1024), nullable=True)
+
+    created_by = relationship('User', foreign_keys=[created_by_user_id])
+    reviewed_by = relationship('User', foreign_keys=[reviewed_by_user_id])
+    
+
+class SchemaCreate(Base):
+    __tablename__ = 'schema_create_history'
+    id = Column(Integer, primary_key=True)
+    change_id = Column(Integer, ForeignKey('changes.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    slug = Column(String(128), nullable=False)
+    reviewable = Column(Boolean, nullable=False)
+
+    change = relationship('Change')
+
+
+class SchemaUpdate(Base):
+    __tablename__ = 'schema_upd_history'
+    id = Column(Integer, primary_key=True) 
+    change_id = Column(Integer, ForeignKey('changes.id'), nullable=False)
+    schema_id = Column(Integer, ForeignKey('schemas.id'), nullable=False)
+    new_name = Column(String(128), nullable=True)
+    old_name = Column(String(128), nullable=True)
+    new_slug = Column(String(128), nullable=True)
+    old_slug = Column(String(128), nullable=True)
+    new_reviewable = Column(Boolean, nullable=True)
+    old_reviewable = Column(Boolean, nullable=True)
+    new_deleted = Column(Boolean, nullable=True)
+    old_deleted = Column(Boolean, nullable=True)
+
+    change = relationship('Change')
+    schema = relationship('Schema')
+
+
+class AttributeUpdate(Base):
+    __tablename__ = 'attr_upd_history'
+    id = Column(Integer, primary_key=True)
+    change_id = Column(Integer, ForeignKey('changes.id'), nullable=False)
+    attribute_id = Column(Integer, ForeignKey('attributes.id'), nullable=False)
+    new_name = Column(String(128), nullable=True)
+    required = Column(Boolean, nullable=False)
+    unique = Column(Boolean, nullable=False)
+    list = Column(Boolean, nullable=False)
+    key = Column(Boolean, nullable=False)
+    description = Column(String(128), nullable=True)
+    bind_to_schema = Column(Integer, ForeignKey('schemas.id'), nullable=True)
+
+    change = relationship('Change')
+    attribute = relationship('Attribute')
+
+
+class AttributeCreate(Base):
+    __tablename__ = 'attr_create_history'
+    id = Column(Integer, primary_key=True)
+    change_id = Column(Integer, ForeignKey('changes.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    type = Column(Enum(AttrType), nullable=False)
+    required = Column(Boolean, nullable=False)
+    unique = Column(Boolean, nullable=False)
+    list = Column(Boolean, nullable=False)
+    key = Column(Boolean, nullable=False)
+    description = Column(String(128), nullable=True)
+    bind_to_schema = Column(Integer, ForeignKey('schemas.id'), nullable=True)
+
+    change = relationship('Change')
+
+
+class AttributeDelete(Base):
+    __tablename__ = 'attr_delete_history'
+    id = Column(Integer, primary_key=True)
+    change_id = Column(Integer, ForeignKey('changes.id'), nullable=False)
+    attribute_id = Column(Integer, ForeignKey('attributes.id'), nullable=False)
+
+    change = relationship('Change')
+    attribute = relationship('Attribute')
+
+
+class EntityCreate(Base):
+    __tablename__ = 'entity_create_history'
+    id = Column(Integer, primary_key=True)
+    change_id = Column(Integer, ForeignKey('changes.id'), nullable=False)
+    schema_id = Column(Integer, ForeignKey('schemas.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    slug = Column(String(128), nullable=False)
+
+    change = relationship('Change')
+    schema = relationship('Schema')
+
+
+class EntityUpdate(Base):
+    __tablename__ = 'entity_update_history'
+    id = Column(Integer, primary_key=True)
+    change_id = Column(Integer, ForeignKey('changes.id'), nullable=False)
+    entity_id = Column(Integer, ForeignKey('entities.id'), nullable=False)
+    new_name = Column(String(128), nullable=True)
+    new_slug = Column(String(128), nullable=True)
+    old_name = Column(String(128), nullable=True)
+    old_slug = Column(String(128), nullable=True)
+    new_deleted = Column(Boolean, nullable=True)
+    old_deleted = Column(Boolean, nullable=True)
+
+    change = relationship('Change')
+    entity = relationship('Entity')
+
+
+class ValueUpdate(Base):
+    __tablename__ = 'value_update_history'
+    id = Column(Integer, primary_key=True)
+    change_id = Column(Integer, ForeignKey('changes.id'), nullable=False)
+    entity_id = Column(Integer, ForeignKey('entities.id'))
+    attribute_id = Column(Integer, ForeignKey('attributes.id'), nullable=False)
+    new_value = Column(String, nullable=True)
+    old_value = Column(String, nullable=True) 
+
+    change = relationship('Change')
+    entity = relationship('Entity')
+    attribute = relationship('Attribute')
