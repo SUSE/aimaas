7cc999c3a27e17f3c434442c8551f4086c99c0e9
Nikita P <36228624+commanderprice@users.noreply.github.com>
2021-11-30 17:12:51 +0300

    Add generation of API description

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,13 +1,16 @@
 from typing import List
+# from datetime import timedelta
 
-from fastapi import FastAPI
+from fastapi import FastAPI#, Depends, HTTPException, status
+# from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session, subqueryload
 
-from . import database
+from . import database, crud, dynamic_routes
 from .dynamic_routes import create_dynamic_router
-from .models import Schema, AttributeDefinition
-from .general_routes import router
+from .models import Schema, AttributeDefinition, BoundFK
+
 
 
 def load_schemas() -> List[models.Schema]:
@@ -30,10 +33,46 @@ def load_schemas() -> List[models.Schema]:
         db.close()
 
 
+def generate_api_description() -> str:
+    description = '# Filters\n\n**Filters list**:'
+    for filter, desc in dynamic_routes.FILTER_DESCRIPTION.items():
+        description += '\n* `{}` - {}'.format(filter, desc)
+
+    description += '\n\n**Available filters for each type**:'
+    for type, filters in crud.ALLOWED_FILTERS.items():
+        description += '\n* `{}`: {}'.format(type.name, ', '.join([f'`{i}`' for i in filters]))
+    return description
+
+
 def create_app() -> FastAPI:
-    app = FastAPI()
+    from .general_routes import router
+    
+    app = FastAPI(description=generate_api_description())
     schemas = load_schemas()
     for schema in schemas:
         create_dynamic_router(schema=schema, app=app, get_db=database.get_db)
     app.include_router(router)
+
+    # origins = ['*']
+    # app.add_middleware(CORSMiddleware,
+    #     allow_origins=origins,
+    #     allow_credentials=True,
+    #     allow_methods=['*'],
+    #     allow_headers=['*'])
+
+    # from . import auth
+    # @app.post("/token", response_model=auth.Token)
+    # async def login_for_access_token(db: Session = Depends(database.get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    #     user = auth.authenticate_user(db, form_data.username, form_data.password)
+    #     if not user:
+    #         raise HTTPException(
+    #             status_code=status.HTTP_401_UNAUTHORIZED,
+    #             detail="Incorrect username or password",
+    #             headers={"WWW-Authenticate": "Bearer"},
+    #         )
+    #     access_token_expires = timedelta(minutes=auth.ACCESS_TOKEN_EXPIRE_MINUTES)
+    #     access_token = auth.create_access_token(
+    #         data={"sub": user.username}, expires_delta=access_token_expires
+    #     )
+    #     return {"access_token": access_token, "token_type": "bearer"}
     return app
