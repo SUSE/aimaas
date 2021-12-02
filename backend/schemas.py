02bdc6e8b697b1672c4dd39857f75b777c20e4ce
Nikita P <36228624+commanderprice@users.noreply.github.com>
2021-12-02 11:20:23 +0300

    Update slug validator to skip None

--- backend/schemas.py
+++ backend/schemas.py
@@ -61,6 +61,8 @@ class AttrDefUpdateSchema(AttributeDefinitionBase):
 
 
 def validate_slug(cls, slug: str):
+    if slug is None:
+        return slug
     if re.match('^[a-zA-Z]+[0-9]*(-[a-zA-Z0-9]+)*$', slug) is None:
         raise ValueError(f'`{slug}` is invalid value for slug field')
     return slug
