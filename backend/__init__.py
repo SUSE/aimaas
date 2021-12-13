0dadf14e3360ae87a97bb7908ba8b229e5b36923
Nikita P <36228624+commanderprice@users.noreply.github.com>
2021-12-13 15:58:43 +0300

    Add login route

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,9 @@
 from typing import List
-# from datetime import timedelta
+from datetime import timedelta
 
-from fastapi import FastAPI#, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-# from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session, subqueryload
 
@@ -60,19 +60,20 @@ def create_app() -> FastAPI:
         allow_methods=['*'],
         allow_headers=['*'])
 
-    # from . import auth
-    # @app.post("/token", response_model=auth.Token)
-    # async def login_for_access_token(db: Session = Depends(database.get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    #     user = auth.authenticate_user(db, form_data.username, form_data.password)
-    #     if not user:
-    #         raise HTTPException(
-    #             status_code=status.HTTP_401_UNAUTHORIZED,
-    #             detail="Incorrect username or password",
-    #             headers={"WWW-Authenticate": "Bearer"},
-    #         )
-    #     access_token_expires = timedelta(minutes=auth.ACCESS_TOKEN_EXPIRE_MINUTES)
-    #     access_token = auth.create_access_token(
-    #         data={"sub": user.username}, expires_delta=access_token_expires
-    #     )
-    #     return {"access_token": access_token, "token_type": "bearer"}
+    from . import auth
+    from .schemas.auth import Token
+    @app.post('/login', response_model=Token)
+    async def login(db: Session = Depends(database.get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+        user = auth.authenticate_user(db, form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Incorrect username or password',
+                headers={'WWW-Authenticate': 'Bearer'},
+            )
+        expires = timedelta(minutes=auth.s.token_exp_minutes)
+        access_token = auth.create_access_token(
+            data={'sub': user.username}, expires_delta=expires
+        )
+        return {'access_token': access_token, 'token_type': 'bearer'}
     return app
