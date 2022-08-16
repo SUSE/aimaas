ee47db9409b445870374063dab1d9abea48b180d
Leon Schröder <lschroeder@suse.com>
2022-08-16 10:27:34 +0200

    Added link to breadcumb home icon (#85)

--- frontend/src/components/layout/breadcrumbs.vue
+++ frontend/src/components/layout/breadcrumbs.vue
@@ -2,7 +2,9 @@
   <nav aria-label="breadcrumb">
       <ol class="breadcrumb bg-light px-4 py-2 rounded">
         <li class="breadcrumb-item" aria-current="page">
-          <i class="eos-icons">home</i>
+          <RouterLink class="navbar-brand" :to="{name: 'schema-list'}">
+            <i class="eos-icons">home</i>
+          </RouterLink>
         </li>
         <slot></slot>
       </ol>
