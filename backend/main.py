fb9ac2a427fb0ab6c6b499a4660a46cbbc6c829b
Andreas Hasenkopf <ahasenkopf@suse.de>
2023-06-02 10:36:56 +0200

    Removed version constraint of `pydantic`
    The constraint was introduced in #74 due to pydantic/pydantic#4131. The fix for pydantic has been
    released and the constraint is now superfluous.
    
    This version constraint implicitly limited `fastapi-pagination` to v0.9.3, which now also gets
    updated (to v0.11.4). However, it cannot be upgraded to the latest v0.12 series, due to #146.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
+from fastapi_pagination import add_pagination
+
 from . import create_app, models
 from .database import engine
 
 
 models.Base.metadata.create_all(engine)
 app = create_app()
+add_pagination(app)
