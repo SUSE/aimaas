ab58a106e3c80c0610275e62a13adc2c92ad9e5f
Teodora Pavlova <27234545+TeodoraPavlova@users.noreply.github.com>
2024-04-05 10:41:00 +0300

    Fix DateTime attribute field does not render value on Entity edit

--- frontend/src/components/inputs/DateTime.vue
+++ frontend/src/components/inputs/DateTime.vue
@@ -1,7 +1,9 @@
 <template>
   <BaseInput :label="label" :args="args" :vertical="vertical" :required="required">
     <template v-slot:field>
-      <input class="form-control" type="datetime-local" :value="modelValue" @input="onInput"
+      <input class="form-control" type="datetime-local"
+             :value="modelValue && convertToDateTimeLocalString(modelValue)"
+             @input="onInput"
              v-bind="args"/>
     </template>
   </BaseInput>
@@ -18,6 +20,19 @@ export default {
     onInput(event) {
       this.$emit("update:modelValue", event.target.value);
     },
+    convertToDateTimeLocalString(date) {
+      if (!(date instanceof Date)) {
+        date = new Date(date)
+      }
+
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1).toString().padStart(2, "0");
+      const day = date.getDate().toString().padStart(2, "0");
+      const hours = date.getHours().toString().padStart(2, "0");
+      const minutes = date.getMinutes().toString().padStart(2, "0");
+
+      return `${year}-${month}-${day}T${hours}:${minutes}:00`;
+    }
   },
 };
 </script>
